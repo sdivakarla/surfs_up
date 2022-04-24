e5f1164e9a905616a4afdf875b17ad65e8dd919f
sdivakarla <sarah.divakarla@gmail.com>
2022-04-23 20:27:23 -0400

    Flask installed

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+
+
+
+from flask import Flask
+
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return 'Hello world'
+
+# export FLASK_APP=app.py
+# set FLASK_APP=app.py
+# flask run
